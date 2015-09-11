d1f87a5b63c048ddc4cd910cca1b6e919dc82bb1
y_okada <yasokada@gmail.com>
2015-09-12 07:23:20 +0900

    worked

---
+++ Assets/FindParentCanvas.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class FindParentCanvas : MonoBehaviour {
+	
+	string getMyParentCanvasName(GameObject panel) {
+		// 1. one upper
+		GameObject parentGO = panel.transform.parent.gameObject;
+		if (parentGO.GetComponent<Canvas> () != null) {
+			return parentGO.name;
+		}
+			       
+		// 2. two uppers
+		GameObject grandParentGO = parentGO.transform.parent.gameObject;
+		if (grandParentGO.GetComponent<Canvas> () != null) {
+			return grandParentGO.name;
+		}
+
+		return ""; // not found
+	}
+
+	void Start () {
+		GameObject myPanel;
+
+		myPanel = GameObject.Find ("Panel_1_1");
+		string canvasNams = getMyParentCanvasName (myPanel);
+		Debug.Log (canvasNams + " is parent canvas of " + myPanel.name);
+	}	
+}
